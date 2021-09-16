359e812b6876aa1bea103a6a499dc4909c6526b4
Stephen Ball <sdball@gmail.com>
2021-09-16 10:54:01 -0400

    ✏ wait-for

--- .github/actions/wait-for/index.js
+++ .github/actions/wait-for/index.js
@@ -7,53 +7,37 @@ const logFatal = (msg) => {
   return process.exit(1);
 };
 
-const fetchCurrentRun = async (octokit, owner, repo) => {
-  const runId = process.env['GITHUB_RUN_ID'];
-  let response;
-  try {
-    response = await octokit.rest.actions.getWorkflowRun({
-      owner,
-      repo,
-      run_id: runId,
-    });
-  } catch (err) {
-    logFatal('Failed to query current workflow run');
-  }
-  core.debug('Current Run');
-  core.debug(response.data);
-  return response.data;
-};
-
-const fetchWorkflowRuns = async (octokit, owner, repo, branch, workflowFile, status) => {
-  core.info(`fetchWorkflowRuns.listWorkflowRuns branch.${branch} status.${status}`);
-  const params = {
-      owner,
-      repo,
-      workflow_id: workflowFile,
-      branch,
-      status,
-      per_page: 3, // we only need to check to see if there are matching runs
-  };
-  core.debug(params);
-  let response;
-  try {
-    response = await octokit.rest.actions.listWorkflowRuns(params);
-  } catch (err) {
-    logFatal('Failed to query workflow runs');
-  }
-  core.debug(response.data);
-  return response.data;
-}
-
-async function cancelWorkflowRun(octokit, owner, repo, runId) {
-  core.info(`Cancelling workflow run ${runId}`);
-  const response = await octokit.rest.actions.cancelWorkflowRun({
+async function fetchCheckRuns(client, owner, repo, ref) {
+  console.log('waitFor.fetchCheckRuns');
+  const response = await client.checks.listForRef({
     owner,
     repo,
-    run_id: runId,
+    ref,
   });
-  core.debug(response);
-  core.setOutput('this_run_cancelled', true);
+  return response.data.check_runs;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function checkComplete(client, owner, repo, ref, checkName, waitInterval) {
+  const waitIntervalMillis = waitInterval * 1000;
+  while(true) {
+    const allCheckRuns = await fetchCheckRuns(client, owner, repo, ref);
+    console.log(allCheckRuns);
+
+    const matchingCheck = allCheckRuns.filter(c => { return c.name == checkName });
+    console.log(matchingCheck);
+
+    const completed = matchingCheck.filter(c => { return c.status == 'completed' });
+    if (completed.length) {
+      console.log('Check completed');
+      return completed[0];
+    }
+    console.log('Check not completed, sleeping for ${waitIntervalMillis}ms');
+    sleep(waitIntervalMillis);
+  }
 }
 
 async function run() {
@@ -61,50 +45,35 @@ async function run() {
   const ref = core.getInput('ref', { required: true });
   const checkName = core.getInput('check_name', { required: true });
   const waitInterval = core.getInput('wait_interval');
+  const allowedConclusions = core.getInput('allowed_conclusions').split(/,\s+/);
 
   console.log({
     githubToken,
     ref,
     checkName,
     waitInterval,
+    allowedConclusions,
   });
 
   const octokit = github.getOctokit(githubToken);
 
   const [owner, repo] = process.env['GITHUB_REPOSITORY'].split('/');
 
-  const response = await octokit.rest.checks.listForRef({
-    owner,
-    repo,
-    ref,
-  });
+  console.log(`waitFor.run.beginWait owner=${owner} repo=${repo} ref=${ref} check=${checkName} waitInterval=${waitInterval}`);
+  console.log(`::group::waitFor ${checkName}`)
+  const completed = await checkComplete(octokit.rest, owner, repo, ref, checkName, waitInterval);
+  console.log('::endgroup::');
+  console.log(`waitFor.run.finishWait owner=${owner} repo=${repo} ref=${ref} check=${checkName} waitInterval=${waitInterval}`);
 
-  console.log(response.data);
-
-  // const currentRun = await fetchCurrentRun(octokit, owner, repo);
-
-  // // if ANY kind of workflow run follows this run then we'll cancel because we're out of date
-  // const queuedWorkflowRuns = await fetchWorkflowRuns(octokit, owner, repo, currentRun.head_branch, workflowFile, 'queued');
-  // const inProgressWorkflowRuns = await fetchWorkflowRuns(octokit, owner, repo, currentRun.head_branch, workflowFile, 'in_progress');
-  // const completedWorkflowRuns = await fetchWorkflowRuns(octokit, owner, repo, currentRun.head_branch, workflowFile, 'completed');
-
-  // const followingRunIds = [
-  //   ...queuedWorkflowRuns.workflow_runs,
-  //   ...inProgressWorkflowRuns.workflow_runs,
-  //   ...completedWorkflowRuns.workflow_runs,
-  // ].map(r => r.id).filter(id => id > currentRun.id);
-
-  // core.info(`THIS is Build ${currentRun.run_number} / Run ${currentRun.id}`);
-
-  // if (followingRunIds.length > 0) {
-  //   core.info(`Following run IDs are ${followingRunIds}`);
-  //   // there are queued runs after this one
-  //   // we only cancel ourselves to avoid edges like cancelling later runs that SHOULD run
-  //   core.info('There are following runs so we are out of date: cancelling this run');
-  //   await cancelWorkflowRun(octokit, owner, repo, currentRun.id);
-  // } else {
-  //   core.info('Did not find any following runs: proceeding with this run');
-  // }
+  console.log(`waitFor.run.checkComplete conclusion=${completed.conclusion}`);
+  if (allowedConclusions.includes(completed.conclusion)) {
+    console.log('waitFor.run.conclusionAccepted');
+    return;
+  } else {
+    console.log('waitFor.run.conclusionRejected');
+    console.log(`::error::Rejected conclusion for ${checkName}`);
+    return process.exit(1);
+  }
 }
 
 run().catch((err) => {
